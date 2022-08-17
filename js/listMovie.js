9e9cb0010350bbc1791e02eb639218ae10ca6838
Quang Phạm Hữu <ilove060902@gmail.com>
2022-08-17 16:18:17 +0700

    fix image display

--- js/listMovie.js
+++ js/listMovie.js
@@ -11,23 +11,21 @@ function allMovies() {
 
   function renderMovies(data) {
     const { results } = data;
-    console.log(results);
     var htmls = results.map((movie, index) => {
       return `
-            <li class="movie__item movie__item--${movie.id}">
-                <span class="movie__item-category">
-                    ${movie.vote_average} <i class='bx bxs-star'></i>
-                </span>
-                <a class="movie__item-avt">
-                    <img src="https://image.tmdb.org/t/p/original${
-                      movie.poster_path
-                    }" alt="" title="">
-                </a>
-                <a class="movie__item-name">
-                    <p>${movie.title} (${movie.release_date.slice(0, 4)})</p>
-                </a>
-                
-            </li>`;
+        <li class="movie__item movie__item--${movie.id}">
+            <span class="movie__item-category">
+                ${movie.vote_average} <i class='bx bxs-star'></i>
+            </span>
+            <a class="movie__item-avt">
+              <img src="https://image.tmdb.org/t/p/original${
+                movie.poster_path
+              }" alt=""/>
+            </a>
+            <a class="movie__item-name">
+                <p>${movie.title} (${movie.release_date.slice(0, 4)})</p>
+            </a>
+        </li>`;
     });
     movieBlock.forEach(function (movie) {
       movie.innerHTML = htmls;
--- js/topMovie.js
+++ js/topMovie.js
@@ -1,22 +1,21 @@
 function topRated() {
-    var $ = document.querySelector.bind(document);
-    var $$ = document.querySelectorAll.bind(document);
+  var $ = document.querySelector.bind(document);
+  var $$ = document.querySelectorAll.bind(document);
 
-    const topRate = $$('.bar__list')
+  const topRate = $$(".bar__list");
 
-    function getTopRate(callback) {
-        var apiTop = `https://api.themoviedb.org/3/movie/top_rated?api_key=0edf7c2e352307b3adc4dd446193177f&language=en-US&page=1`
+  function getTopRate(callback) {
+    var apiTop = `https://api.themoviedb.org/3/movie/top_rated?api_key=0edf7c2e352307b3adc4dd446193177f&language=en-US&page=1`;
 
-        fetch(apiTop)
-            .then(response => response.json())
-            .then(callback)
-    }
+    fetch(apiTop)
+      .then((response) => response.json())
+      .then(callback);
+  }
 
-    function renderTopRated(data) {
-        console.log(data.results)
-        const imgUrl = 'https://image.tmdb.org/t/p/original'
-        const htmls = data.results.map(result => {
-            return `
+  function renderTopRated(data) {
+    const imgUrl = "https://image.tmdb.org/t/p/original";
+    const htmls = data.results.map((result) => {
+      return `
                 <li class="bar__item">
                     <a class="bar__item-link" href="">
                         <img src="${imgUrl}${result.poster_path}" alt="" class="bar__item-img">
@@ -26,10 +25,10 @@ function topRated() {
                         </div>
                     </a>
                 </li>
-                `
-        })
-        topRate.forEach(top => top.innerHTML = htmls)
-    }
-    getTopRate(renderTopRated)
+                `;
+    });
+    topRate.forEach((top) => (top.innerHTML = htmls));
+  }
+  getTopRate(renderTopRated);
 }
 export default topRated;
